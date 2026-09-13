e8ded67e3989a370ca2ef6215edb63db83c85148
myraclez <myraclezdev@gmail.com>
2026-09-13 22:02:37 +0200

    Made AuctionEvent monitor only so no changes can be made

--- src/main/java/net/klyde/klydeauctionsapi/event/AuctionEvent.java
+++ src/main/java/net/klyde/klydeauctionsapi/event/AuctionEvent.java
@@ -3,7 +3,10 @@ package net.klyde.klydeauctionsapi.event;
 import net.klyde.klydeauctionsapi.model.AuctionModel;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public abstract class AuctionEvent extends Event {
 
@@ -13,8 +16,32 @@ public abstract class AuctionEvent extends Event {
 		this.auctionModel = auctionModel;
 	}
 
-	public AuctionModel getAuctionModel() {
-		return auctionModel;
+	public UUID getAuctionId() {
+		return auctionModel.getId();
+	}
+
+	public UUID getOwnerUuid() {
+		return auctionModel.getOwner();
+	}
+
+	public ItemStack getItemCopy() {
+		return auctionModel.getItem().clone();
+	}
+
+	public double getPrice() {
+		return auctionModel.getPrice();
+	}
+
+	public long getCreationTimeMillis() {
+		return auctionModel.getCreatedAt();
+	}
+
+	public long getExpiredMillis() {
+		return auctionModel.getExpiredAt();
+	}
+
+	public AuctionModel.AuctionState getState() {
+		return auctionModel.getState();
 	}
 
 	private static final HandlerList HANDLERS = new HandlerList();
